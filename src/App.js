e2e71f1476b0b067ed4607e8c1daa8cf641ceacf
pj-wise <51010291+pj-wise@users.noreply.github.com>
2019-08-02 15:17:40 -0400

    fetching API data with axios and using async await with componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
-import "./App.css";
 import Navbar from "./Components/Layout/Navbar";
 import Users from "./Components/Users/Users";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get("https://api.github.com/users");
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
-        <Users />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -2,38 +2,21 @@ import React, { Component } from "react";
 import UserItem from "./UserItem";
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "id",
-        login: "mojombo",
-        avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-      },
-      {
-        id: "id",
-        login: "defunkt",
-        avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
-        html_url: "https://github.com/defunkt"
-      },
-      {
-        id: "id",
-        login: "pjhyett",
-        avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
-        html_url: "https://github.com/pjhyett"
-      }
-    ]
-  };
-
   render() {
     return (
-      <div>
-        {this.state.users.map(user => {
+      <div style={userStyle}>
+        {this.props.users.map(user => {
           return <UserItem key={user.id} user={user} />;
         })}
       </div>
     );
   }
 }
+
+const userStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat( 3, 1fr)",
+  gridGap: "1rem"
+};
 
 export default Users;
