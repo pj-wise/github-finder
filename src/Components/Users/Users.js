f7b93d5c0b6fc2fd11ba2a40fbad6aacb10bb6c8
pj-wise <51010291+pj-wise@users.noreply.github.com>
2019-08-03 21:19:29 -0400

    added proptypes

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
 import UserItem from "./UserItem";
 import Spinner from "../Layout/Spinner";
+import PropTypes from "prop-types";
 
 const Users = ({ users, loading }) => {
   if (loading) {
     return <Spinner />;
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map(user => {
+          return <UserItem key={user.id} user={user} />;
+        })}
+      </div>
+    );
   }
+};
 
-  return (
-    <div style={userStyle}>
-      {users.map(user => {
-        return <UserItem key={user.id} user={user} />;
-      })}
-    </div>
-  );
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
 const userStyle = {
