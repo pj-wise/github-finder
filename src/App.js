f7e0878a579eeacc8a6ea68d5d9f25689ca5eed8
pj-wise <51010291+pj-wise@users.noreply.github.com>
2019-08-04 00:05:29 -0400

    created clear button that shows only when user array length is greater than 0

--- src/App.js
+++ src/App.js
@@ -11,20 +11,23 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
-    this.setState({ loading: true });
+  // UNCOMMENT FOR USERS TO RENDER AT START
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
 
-    const res = await axios.get(
-      `https://api.github.com/users?client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${
+  //       process.env.REACT_APP_GITHUB_CLIENT_ID
+  //     }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
 
-    this.setState({ users: res.data, loading: false });
-  }
+  //   this.setState({ users: res.data, loading: false });
+  // }
 
   //SEARCH GITHUB USERS..... PROP DRILLING
   searchUsers = async text => {
+    this.setState({ loading: true });
+
     const res = await axios.get(
       // SAME call as above ^ changed endpoint dynamic based on text
       `https://api.github.com/search/users?q=${text}&client_id=${
@@ -35,12 +38,19 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  //CLEAR USERS FROM STATE
+  clearUsers = () => this.setState({ users: [], loading: false });
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/Components/Users/Search.js
+++ src/Components/Users/Search.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Search extends Component {
   state = {
     text: ""
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
   };
 
   onSubmit = e => {
@@ -38,6 +45,14 @@ class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {this.props.showClear && ( // && means if this.props.showClear is true then show button
+          <button
+            className="btn btn-light btn-block"
+            onClick={this.props.clearUsers}
+          >
+            Clear
+          </button>
+        )}
       </div>
     );
   }
